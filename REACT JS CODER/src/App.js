bcd96bf26cc5d5c879c0fee3e6e3e2cfb0356ca1
Juli Sal <juli.salerno@hotmail.com>
2021-10-10 05:15:21 -0300

    add navbar 2nd challenge

--- REACT JS CODER/src/App.js
+++ REACT JS CODER/src/App.js
@@ -3,16 +3,13 @@ function App() {
     <div className="App">
       <header className="App-header">
        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Texto
+        
         </a>
       </header>
     </div>
@@ -20,3 +17,4 @@ function App() {
 }
 
 export default App;
+
