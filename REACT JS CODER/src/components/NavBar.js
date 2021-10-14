b9f2f81121292ca1e74d4d9feb5f167631a64293
Juli Sal <juli.salerno@hotmail.com>
2021-10-13 22:15:28 -0300

    Add NavBar

---
+++ REACT JS CODER/src/components/NavBar.js
@@ -0,0 +1,42 @@
+function NavBar() {
+    return (
+      <div className="Navbar">
+        <header className="App-header">
+         
+          <a
+            className="NavBar-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          
+          </a>
+        </header>
+      </div>
+    );
+  }
+
+<><div id="root"></div><ul id="dropdown1" class="dropdown-content">
+    <li><a href="#!">Sombras</></li>
+    <li><a href="#!">Bases</a></li>
+    <li class="divider"></li>
+    <li><a href="#!">Pinceles</a></li>
+</ul><nav>
+        <div class="nav-wrapper">
+            <a href="#!" class="brand-logo">Shulai TIENDA</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#">Paletas Nacionales</a></li>
+                <li><a href="#">Pre Makeup</a></li>
+                <li><a href="#">Pinceles</a></li>
+
+            </ul>
+        </div>
+    </div></>
+    </nav>
+      </div>
+    </nav>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+ 
+  export default NavBar;
+  
+  
