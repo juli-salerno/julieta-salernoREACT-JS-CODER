2f1c84839dfd86c46736696c5fca8ee799d6d3d4
Juli Sal <juli.salerno@hotmail.com>
2021-10-17 21:08:03 -0300

    changes NavBar once more

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,42 +1,49 @@
+var React = require('react');
+
+var NavBar = React.createClass({
+    render: function (){
+        return<div>
+            navbar
+        </div>
+    }
+});
+
+module.exports = NavBar;
+
 function NavBar() {
     return (
-      <div className="Navbar">
-        <header className="App-header">
-         
-          <a
+             <div className="Navbar">
+                <header className="App-header">
+                <a
             className="NavBar-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
-          >
-          
-          </a>
-        </header>
-      </div>
-    );
-  }
-
-<><div id="root"></div><ul id="dropdown1" class="dropdown-content">
+                >
+                <div id="root"></div>
+         <ul id="dropdown1" class="dropdown-content">
     <li><a href="#!">Sombras</></li>
     <li><a href="#!">Bases</a></li>
     <li class="divider"></li>
     <li><a href="#!">Pinceles</a></li>
-</ul><nav>
+        </ul>
+        <nav>
         <div class="nav-wrapper">
             <a href="#!" class="brand-logo">Shulai TIENDA</a>
             <ul class="right hide-on-med-and-down">
                 <li><a href="#">Paletas Nacionales</a></li>
                 <li><a href="#">Pre Makeup</a></li>
                 <li><a href="#">Pinceles</a></li>
+                <li><a href="#">Stock</a></li>
 
             </ul>
-        </div>
-    </div></>
-    </nav>
+      
       </div>
     </nav>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
- 
+
+</a>
+        </header>
+      </div>
+    );
   export default NavBar;
-  
-  
+
